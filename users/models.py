ab59dd44640f3387e287ba8710325ed5ea2d8709
Conor Cunningham <conor.cunningham@gmail.com>
2019-10-26 18:27:12 +0200

    added S3 for media folders

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class CustomUser(AbstractUser):
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image)         # removed .path from image
+            img.save(self.image.path)         # removed .path from image
 
     def __str__(self):
         return f"{self.username}'s Profile"
