dbedbfde72b26e212dd8f3c5ce4ed66a2ac6c644
Conor Cunningham <conor.cunningham@gmail.com>
2020-02-27 21:30:56 +0100

    added sitemap

--- cunningtek_project/settings.py
+++ cunningtek_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.sitemaps',
     'storages',
 
     # my apps
--- cunningtek_project/urls.py
+++ cunningtek_project/urls.py
@@ -1,25 +1,18 @@
-"""cunningtek_project URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 from blog.views import google
+from blog.models import Post
+
+post_dict = {
+    'queryset': Post.objects.all(),
+}
 
 urlpatterns = [
     path('googled7c5cb564f623934.html', google, name='google'),
@@ -30,7 +23,9 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('users.urls')),
-    path('martor/', include('martor.urls')),
+    path('sitemap.xml', sitemap,  # new
+         {'sitemaps': {'blog': GenericSitemap(post_dict, priority=0.6)}},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 
